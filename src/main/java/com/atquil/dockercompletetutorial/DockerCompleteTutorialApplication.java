b605be5b07a7ed72ce71fd5df6ff8a544c2262c8
ATQUIL <creator.atquil@gmail.com>
2026-01-04 09:18:34 +0530

    Docker Tutorial Series

---
+++ src/main/java/com/atquil/dockercompletetutorial/DockerCompleteTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.atquil.dockercompletetutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerCompleteTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerCompleteTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atquil/dockercompletetutorial/DockerCompleteTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atquil.dockercompletetutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerCompleteTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
